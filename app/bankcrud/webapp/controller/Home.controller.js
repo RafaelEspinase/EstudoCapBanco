b60ae7d41d6e4c00e508e8638773b0755231bf88
Rafael Espinase <rafaelespinase@hotmail.com>
2025-02-20 23:35:47 +0000

    populando tabela extract e expondo na home com formatter para valores de data e reais

--- app/bankcrud/webapp/controller/Home.controller.js
+++ app/bankcrud/webapp/controller/Home.controller.js
@@ -3,16 +3,21 @@ sap.ui.define([
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
 	"sap/ui/model/json/JSONModel",
+	"../model/formatter"
 ], (Controller,
 	Filter,
 	FilterOperator,
-	JSONModel,) => {
+	JSONModel,
+	formatter) => {
     "use strict";
 
     return Controller.extend("bankcrud.controller.Home", {
+        formatter: formatter,
         async onInit() {
             let dataAccounts = await this.onLoadAccounts();
-            this.getView().setModel(new JSONModel(dataAccounts), "homeModel");
+            this.getView().setModel(new JSONModel(dataAccounts), "accountModel");
+            let dataExtract = await this.onLoadExtract();
+            this.getView().setModel(new JSONModel(dataExtract), "extractModel");
         },
         async onLoadAccounts(){
             var oModel = this.getOwnerComponent().getModel(); 
@@ -34,6 +39,33 @@ sap.ui.define([
                         const errModel = {
                             Error: oError.responseText,
                             Entity: '/Accounts'
+                        };
+                        reject(errModel);
+                    }
+                });
+            });
+        },
+        async onLoadExtract(){
+            var oModel = this.getOwnerComponent().getModel(); 
+            let account_ID = this.getView().getModel("accountModel").getData().ID;
+            let filter = new Filter("account_ID", FilterOperator.EQ, account_ID);
+
+            return new Promise(function (resolve, reject) {
+                oModel.read("/Extract", {
+                    filters: [filter],
+                    // urlParameters: {
+                    //     "$expand": [
+                    //         "customer", 
+                    //         "customer/addresses",
+                    //     ].join(',') 
+                    // },
+                    success: function (oData) {
+                        resolve(oData.results);
+                    },
+                    error: function (oError) {
+                        const errModel = {
+                            Error: oError.responseText,
+                            Entity: '/Extract'
                         };
                         reject(errModel);
                     }
---
+++ app/bankcrud/webapp/model/formatter.js
@@ -0,0 +1,34 @@
+/* global moment:true */
+sap.ui.define([], function () {
+    "use strict";
+    return {
+        convertDateTime: function (value) {
+            const data = new Date(value);
+
+            // Obtendo dia, mês e ano
+            const dia = String(data.getDate()).padStart(2, '0');
+            const mes = String(data.getMonth() + 1).padStart(2, '0'); // Janeiro é 0
+            const ano = data.getFullYear();
+
+            // Obtendo horas, minutos e segundos
+            const horas = String(data.getHours()).padStart(2, '0');
+            const minutos = String(data.getMinutes()).padStart(2, '0');
+            const segundos = String(data.getSeconds()).padStart(2, '0');
+
+            // Formatando para dd/mm/aaaa hh:mm:ss
+            const dataFormatada = `${dia}/${mes}/${ano} as ${horas}:${minutos}:${segundos}`;
+
+            return(dataFormatada); // Saída: 11/04/2024 21:00:00
+        },
+        convertValueBRL: function(value){
+            // Formatando para moeda brasileira (BRL)
+            value = parseInt(value);
+            const valueFormated = value.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+            });
+
+            return(valueFormated); 
+        }
+    };
+  });
